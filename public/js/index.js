760002bf4adc93c503a6cda334680efd08ea69b1
Naman <naman1722000@gmail.com>
2019-12-11 18:27:11 +0530

    custom/broadcast events

---
+++ public/js/index.js
@@ -0,0 +1,13 @@
+let socket = io();
+
+socket.on("connect", ()=>{
+    console.log("Connected to the server");
+})
+
+socket.on("newMessage", function(message){
+    console.log("newMessage : ", message);
+});
+
+socket.on("disconnect", ()=>{
+    console.log("Disconnected from the server");
+})
